7e76a95c8bce8edf99af03f2a234ebdf264d2b9f
Marcos V. H. Pereira <60493029+marcos-hein@users.noreply.github.com>
2021-09-02 20:57:18 -0300

    fist commit

---
+++ src/components/NewTransactionModal/index.tsx
@@ -0,0 +1,120 @@
+import { ChangeEventHandler, FormEvent, InputHTMLAttributes, useState } from "react";
+import Modal from "react-modal";
+import { useTransactions } from "../../hooks/useTransactions";
+
+import closeImg from "../../assets/close.svg";
+import outcomeImg from "../../assets/outcome.svg";
+import incomeImg from "../../assets/income.svg";
+
+import { Container, TransactionTypeContainer, RadioBox } from "./styles";
+
+interface NewTransactionModalProps {
+  isOpen: boolean;
+  onRequestClose: () => void;
+}
+
+export function NewTransactionModal({isOpen, onRequestClose}: NewTransactionModalProps) {
+  const { createTransaction } = useTransactions();
+
+  const [title, setTitle] = useState('');
+  const [category, setCategory] = useState('');
+  const [amount, setAmount] = useState(0);
+  const [type, setType] = useState('deposit');
+
+  async function handleCreateNewTransaction(event: FormEvent) {
+    event.preventDefault();
+
+    await createTransaction({
+      title,
+      amount,
+      category,
+      type
+    })
+    
+    setTitle('');
+    setAmount(0);
+    setCategory('');
+    setType('deposit');
+    onRequestClose();
+  }
+
+  function handleChange(event:React.FormEvent<HTMLInputElement>) {
+    const { value } = event.currentTarget
+    const x = new Intl.NumberFormat('pt-BR', {
+      style: 'currency',
+      currency: 'BRL'
+    }).format(Number(value))
+    // setAmount(x)
+    console.log(x)
+    setAmount(Number(value))
+  }
+  return (
+    <Modal 
+        isOpen={isOpen}
+        onRequestClose={onRequestClose}
+        overlayClassName="react-modal-overlay"
+        className="react-modal-content"
+      >
+        <button
+          type="button"
+          onClick={onRequestClose}
+          className="react-modal-close"
+        >
+          <img src={closeImg} alt="Fechar modal" />
+        </button>
+
+        <Container onSubmit={handleCreateNewTransaction}>
+          <h2>Cadastrar transação</h2>
+
+          <input
+            placeholder="Titulo"
+            value={title}
+            onChange={event => setTitle(event.target.value)}
+          />
+
+          <input
+            type="number"
+            placeholder="Valor"
+            value={new Intl.NumberFormat('pt-BR', {
+              style: 'currency',
+              currency: 'BRL'
+            }).format(Number(amount))}
+            onChange={handleChange}
+          />
+
+          <TransactionTypeContainer>
+            <RadioBox
+              type="button"
+              onClick={() => { setType('deposit') }}
+              isActive={type === 'deposit'}
+              activeColor="green"
+            >
+              <img src={incomeImg} alt="Entrada" />
+              <span>Entrada</span>
+            </RadioBox>
+
+            <RadioBox
+              type="button"
+              onClick={() => { setType('withdraw') }}
+              isActive={type === 'withdraw'}
+              activeColor="red"
+            >
+              <img src={outcomeImg} alt="Saída" />
+              <span>Saída</span>
+            </RadioBox>
+          </TransactionTypeContainer>
+
+          <input
+            placeholder="Categoria"
+            value={category}
+            onChange={event => setCategory(event?.target.value)}
+          />
+
+          <button type="submit">
+            Cadastrar
+          </button>
+        </Container>
+        
+    </Modal>
+  )
+}
